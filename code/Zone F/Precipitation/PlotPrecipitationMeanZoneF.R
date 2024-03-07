fe1da79a6b7c63c6168f4d83397f551f43638aaa
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2024-03-07 10:09:54 -0500

    Add new files and images for Zone A, Zone C, and Zone F

---
+++ code/Zone F/Precipitation/PlotPrecipitationMeanZoneF.R
@@ -0,0 +1,54 @@
+library("ncdf4")
+library("WaveletComp")
+citation("WaveletComp")
+
+# Load the data
+# Historical
+ncin1850 <- nc_open("Historical/dataset/Precipitation/pr_Amon_GISS-E2-H-CC_historical_r1i1p1_185001-190012.nc")
+ncin1901 <- nc_open("Historical/dataset/Precipitation/pr_Amon_GISS-E2-H-CC_historical_r1i1p1_190101-195012.nc")
+ncin1951 <- nc_open("Historical/dataset/Precipitation/pr_Amon_GISS-E2-H-CC_historical_r1i1p1_195101-201012.nc")
+# RCP 4.5
+ncin2006 <- nc_open("RCP 4.5/dataset/Precipitation/pr_Amon_GISS-E2-H-CC_rcp45_r1i1p1_200601-205012.nc")
+ncin2051 <- nc_open("RCP 4.5/dataset/Precipitation/pr_Amon_GISS-E2-H-CC_rcp45_r1i1p1_205101-210012.nc")
+
+# Grab the dimensions
+lon <- ncvar_get(ncin2006, "lon")
+lat <- ncvar_get(ncin2006, "lat")
+
+# NYISO Zone F
+lon_range <- which(lon == 43.75)
+lat_range <- which(lat == -75)
+
+# Subset the longitude and latitude
+lon_subset <- lon[lon_range]
+lat_subset <- lat[lat_range]
+
+# Grab the data
+# Add the different time periods together
+tmp.array <- ncvar_get(ncin1850, "pr")[lon_range, lat_range, ]
+tmp.array <- c(tmp.array, ncvar_get(ncin1901, "pr")[lon_range, lat_range, ])
+tmp.array <- c(tmp.array, ncvar_get(ncin1951, "pr")[lon_range, lat_range, ])
+tmp.array <- c(tmp.array, ncvar_get(ncin2006, "pr")[lon_range, lat_range, ])
+tmp.array <- c(tmp.array, ncvar_get(ncin2051, "pr")[lon_range, lat_range, ])
+
+# Convert the data from kg/m^2/s to g/m^2/hour
+tmp.array <- tmp.array * 3600000
+
+# Close the file
+nc_close(ncin1850)
+nc_close(ncin1901)
+nc_close(ncin1951)
+nc_close(ncin2006)
+nc_close(ncin2051)
+
+# Convert the array to a time series
+tmp.ts <- ts(tmp.array, start = c(1850, 1), frequency = 12)
+
+# Aggregate the data to yearly frequency
+tmp.ts.year <- aggregate(tmp.ts, nfrequency = 1, FUN = mean)
+
+# Create a sequence of years for the x-axis labels
+years <- seq(from = 1850, length.out = length(tmp.ts.year))
+
+# Plot the data with the year labels
+matplot(years, tmp.ts.year, type = "l", xlab = "Year", ylab = "Precipitation (g/m^2/hour)", main = "Precipitation Mean in NYISO Zone F (1850-2100)")
